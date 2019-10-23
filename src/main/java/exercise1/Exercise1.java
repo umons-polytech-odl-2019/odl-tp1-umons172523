fdb4066b3a78fe1010fa0f11f44d447ff3628346
Océane <172523@umons.ac.be>
2019-10-23 15:00:43 +0200

    Ex1 tp2 test réussi

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,16 +1,22 @@
 package exercise1;
 
 public class Exercise1 {
-	static Person createPerson(String name, int age) {
-		// Ajoutez les champs name et age à la classe Person.
-		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
-		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+	static Person createPerson(String name, int age)
+	{
+		// Ok:Ajoutez les champs name et age à la classe Person.
+		// Ok:Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
+		// Ok:Créez des getters publics pour lire ces valeurs une fois la classe construite.
+		//return null; on doit le retirer et creer une personne
+		Person A=new Person(name, age);
+		return A;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+		Person A= new Person(args[0], Integer.parseInt(args[1]));// car on le fait a partir d'une ligne de commande
+		System.out.println(A.getName()+" "+A.getAge());
 	}
 }
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,40 @@
 package exercise1;
 
-class Person {
+class Person
+{
+	private int age;
+	private String name;
+
+	 Person(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+		/*name=myname;
+		age=myage;
+		si on met ca on doit mettre public Person(String myname, int myage)
+		*/
+
+	}// constructeur
+
+	public void Set_name(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void SetAge(int age)
+	{
+		this.age=age;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+
 }
